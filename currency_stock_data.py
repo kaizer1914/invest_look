238f131ac70bb8fde742004e26ec46d7c4edd26b
kirill <kaizer106@gmail.com>
2022-11-07 23:41:14 +0300

    change info request to stock

---
+++ currency_stock_data.py
@@ -0,0 +1,24 @@
+import pandas
+from pandas import DataFrame
+
+
+class CurrencyStockData:
+
+    @staticmethod
+    def get_currencies_df() -> DataFrame:
+        url = 'https://iss.moex.com/iss/statistics/engines/futures/markets/indicativerates/securities.json?iss.meta=off'
+        response_df = pandas.read_json(url)
+        response_df = response_df['securities']
+        response_list = response_df.tolist()
+        response_df = DataFrame(data=response_list[1], columns=response_list[0])
+        return response_df
+
+    @staticmethod
+    def get_currency_course(first_currency: str, second_currency: str = 'RUB') -> float:
+        first_currency = first_currency.upper()
+        second_currency = second_currency.upper()
+        currencies_par = first_currency + '/' + second_currency  # Валютная пара, которая интересует
+        cur_df = CurrencyStockData.get_currencies_df()  # Получаем датафрейм с валютами с Мосбиржи
+        cur_df = cur_df[cur_df['secid'].isin([currencies_par])]  # Находим строку в датафрейме с валютной парой
+        course = cur_df['rate'].values[0]  # Извлекаем значение курса из строки
+        return course
--- share_stock_data.py
+++ share_stock_data.py
@@ -94,23 +94,17 @@ class ShareStockData:
         url = f'https://iss.moex.com/iss/engines/stock/markets/shares/securities/{self.__ticker}' \
               f'/securities.json?iss.meta=off'
         response_df = pandas.read_json(url)
+
         securities_df = response_df['securities']
-        self.__info_df = DataFrame(data=securities_df.data, columns=securities_df.columns)
-        self.__info_df = self.__info_df[self.__info_df['BOARDID'].isin(['TQBR'])]
-        self.__info_df = self.__info_df[['SECID', 'SHORTNAME', 'TRADEDATE']]
-        self.__info_df = self.__info_df.rename(columns={'SECID': 'ticker',
-                                                        'SHORTNAME': 'short_name',
-                                                        'SECNAME': 'long_name',
-                                                        'LOTSIZE': 'lot_size',
-                                                        'FACEVALUE': 'nominal',
-                                                        'DECIMALS': 'decimal_point',
-                                                        'ISIN': 'isin',
-                                                        'ISSUESIZE': 'num_shares',
-                                                        'MINSTEP': 'price_step',
-                                                        'CURRENCYID': 'currency',
-                                                        'SECTYPE': 'sec_type',
-                                                        'LISTLEVEL': 'list_level',
-                                                        })
+        marketdata_df = response_df['marketdata']
+
+        securities_df = DataFrame(data=securities_df.data, columns=securities_df.columns)
+        marketdata_df = DataFrame(data=marketdata_df.data, columns=marketdata_df.columns)
+
+        securities_df = securities_df[securities_df['BOARDID'].isin(['TQBR'])]
+        marketdata_df = marketdata_df[marketdata_df['BOARDID'].isin(['TQBR'])]
+
+        self.__info_df = securities_df.merge(marketdata_df)
         return self.__info_df
 
     def get_sec_history(self):
@@ -118,22 +112,3 @@ class ShareStockData:
 
     def get_info(self):
         return self.__info_df
-
-    @staticmethod
-    def get_currencies_df() -> DataFrame:
-        url = 'https://iss.moex.com/iss/statistics/engines/futures/markets/indicativerates/securities.json?iss.meta=off'
-        response_df = pandas.read_json(url)
-        response_df = response_df['securities']
-        response_list = response_df.tolist()
-        response_df = DataFrame(data=response_list[1], columns=response_list[0])
-        return response_df
-
-    @staticmethod
-    def get_currency_course(first_currency: str, second_currency: str = 'RUB') -> float:
-        first_currency = first_currency.upper()
-        second_currency = second_currency.upper()
-        currencies_par = first_currency + '/' + second_currency  # Валютная пара, которая интересует
-        cur_df = ShareStockData.get_currencies_df()  # Получаем датафрейм с валютами с Мосбиржи
-        cur_df = cur_df[cur_df['secid'].isin([currencies_par])]  # Находим строку в датафрейме с валютной парой
-        course = cur_df['rate'].values[0]  # Извлекаем значение курса из строки
-        return course
